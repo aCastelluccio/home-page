262120334707a618edb075c942132aac854c433c
acastelluccio <acastelluccio@drew.edu>
2018-11-05 18:43:51 -0500

    Updates

---
+++ precache-manifest.c0c44676f01e586835b56946d47afce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "77de17c05e215b8e17ff",
+    "url": "/home-page/static/js/runtime~main.77de17c0.js"
+  },
+  {
+    "revision": "87c6242e9a5b048f007c",
+    "url": "/home-page/static/js/main.87c6242e.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/home-page/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "87c6242e9a5b048f007c",
+    "url": "/home-page/static/css/main.ee4a9d3d.chunk.css"
+  },
+  {
+    "revision": "39649ffa34e9821212fbd9e1529d1032",
+    "url": "/home-page/index.html"
+  }
+];
