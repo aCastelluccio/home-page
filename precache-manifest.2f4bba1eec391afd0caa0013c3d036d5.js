762dbd17a7c7b41bd96d03d4a138e540401c47b8
acastelluccio <acastelluccio@drew.edu>
2018-11-08 09:05:26 -0500

    Updates

---
+++ precache-manifest.2f4bba1eec391afd0caa0013c3d036d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8b39aed848501ce39d115419efbcb2",
+    "url": "/home-page/static/media/cobweb.8c8b39ae.jpg"
+  },
+  {
+    "revision": "77de17c05e215b8e17ff",
+    "url": "/home-page/static/js/runtime~main.77de17c0.js"
+  },
+  {
+    "revision": "a4bb2f785e8c7a6554c3",
+    "url": "/home-page/static/js/main.a4bb2f78.chunk.js"
+  },
+  {
+    "revision": "13e1aea5818a8dfd00dd",
+    "url": "/home-page/static/js/1.13e1aea5.chunk.js"
+  },
+  {
+    "revision": "a4bb2f785e8c7a6554c3",
+    "url": "/home-page/static/css/main.ca9ed474.chunk.css"
+  },
+  {
+    "revision": "8591da5ea23626f772103ae5d660a0d6",
+    "url": "/home-page/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home-page/precache-manifest.bd5e342df9786789627371289165444d.js"
+  "/home-page/precache-manifest.2f4bba1eec391afd0caa0013c3d036d5.js"
 );
 
 workbox.clientsClaim();
